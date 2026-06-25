94afe48233b4bf8061b210e28b2f9b420d80a286
Kadyrzhan23 <112863599+Kadyrzhan23@users.noreply.github.com>
2026-06-25 20:24:44 +0500

    feat(components): add CarSvg component

---
+++ src/components/CarSvg/index.tsx
@@ -0,0 +1,13 @@
+import styles from './index.module.scss';
+
+type Props = {};
+
+const index = ({}: Props) => {
+  return (
+    <div className={styles.root}>
+      
+    </div>
+  );
+};
+
+export default index;
